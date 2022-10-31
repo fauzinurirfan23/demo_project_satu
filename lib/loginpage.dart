0e3f0234a2bb9fbb7cead7c9f3a969016db9fb3c
Fauzi <fauzinurirfan4@gmail.com>
2022-10-31 14:57:20 +0700

    update loginpage

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -12,10 +12,32 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: Center(
-        child: Text('Login Page'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: const Text('Login'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 20,
+            width: double.infinity,
+            color: Colors.amber,
+          ),
+          const SizedBox(height: 10),
+          Container(
+            height: 20,
+            width: double.infinity,
+            color: Colors.green,
+          ),
+          const SizedBox(height: 10),
+          Container(
+            height: 20,
+            width: double.infinity,
+            color: Colors.red,
+          ),
+          const SizedBox(height: 10),
+        ],
       ),
     );
   }
